7801c0fe5d49ef30e00732b715ef57dc438af476
Thien1Nguyen <tnguyen5219@gmail.com>
2023-03-22 21:07:34 -0700

    Next Draft

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-var count1 = 9;
-var count2 = 12;
-var count3 = 9;
+const likeList = document.querySelectorAll(".like")
 
-
-function addCount1(){
-    var countElement = document.querySelector("#like1");
-    count1++;
-    countElement.innerText = count1;
-}
-
-
-function addCount2(){
-    var countElement = document.querySelector("#like2");
-    count2++;
-    countElement.innerText = count2;
-}
-
-function addCount3(){
-    var countElement = document.querySelector("#like3");
-    count3++;
-    countElement.innerText = count3;
+function likeCount(likePosition){
+    likeList[likePosition].innerHTML++;
 }
